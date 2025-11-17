b5f98b472daf38ae26b05c24ed0e24ed6ba7e663
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-11-17 17:15:43 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants.js';
+
+
+
+
+/*
+import express from 'express';
+const app = express();
+
+
+(async()=>{
+    try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`); 
+    app.on("error", (error) => {
+      console.error("Error starting the server:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  }
+     catch (error) {
+      console.error("Error connecting to the database:", error);
+      throw error;
+    }
+})()
+    */
