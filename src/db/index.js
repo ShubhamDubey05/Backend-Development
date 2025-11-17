3d6b527cc1b658c94c7148c595b617e116131360
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-11-17 22:14:47 +0530

    Database connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import {DB_NAME } from '../constants.js';
+
+
+const connectDB = async () => {
+  try {
+    const connectionIntance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log(`Database connected successfully !! DB Host : ${connectionIntance.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
+}
+
+
+export default connectDB;
