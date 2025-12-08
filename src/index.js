6f20cf5f2e966327d00b73816a6c046888469e3d
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-12-08 12:54:33 +0530

    database connection update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
+
 import mongoose from 'mongoose';
 import { DB_NAME } from './constants.js';
 import connectDB from './db/index.js';
 dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  });
+
 
 
 
